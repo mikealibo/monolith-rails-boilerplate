097386128b597c5f62b6d38142e1ea13bda49405
Mike <miksalibo@gmail.com>
2025-03-26 12:38:24 +0800

    Update public_spec to test root path instead of /public/index

--- spec/requests/public_spec.rb
+++ spec/requests/public_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Publics", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/public/index"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
