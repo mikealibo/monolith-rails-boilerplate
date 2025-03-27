721b4a36d59e1eeba437ee5af03049ba56f31a25
Mike <miksalibo@gmail.com>
2025-03-27 09:32:24 +0800

    Add User model with Devise modules and FullNameConcern inclusion

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable
+
+  include FullNameConcern # app/models/concerns/full_name_concern.rb
+end
